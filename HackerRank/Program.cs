5c58bce9ae1e2e1510a95340e36f482d0cebbfd0
Abu Zafor Khairuzzaman <zafor@aplectrum.com>
2020-09-13 16:59:36 +0600

    solve day six problem

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -6,7 +6,28 @@ namespace HackerRank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DaySix();
+        }
+
+        private static void DaySix()
+        {
+            int T = int.Parse(Console.In.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                var inputText = Console.ReadLine();
+                var stringArray = inputText.ToCharArray();
+                var oddText = "";
+                var evenText = "";
+                var index = 0;
+                while (true)
+                {
+                    oddText += stringArray[index++];
+                    if (index > stringArray.Length - 1) break;
+                    evenText += stringArray[index++];
+                    if (index > stringArray.Length - 1) break;
+                }
+                Console.WriteLine($"{oddText} {evenText}");
+            }
         }
     }
 }
